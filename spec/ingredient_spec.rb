ff45d55017277b6a2ba207bcebcc319d9962962c
davisc0801 <davisc1014@gmail.com>
2019-04-10 10:14:38 -0600

    Add tests for ingredient class

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -3,6 +3,24 @@ require './lib/ingredient'
 
 describe Ingredient do
 
+  before(:all) do
+    @cheese = Ingredient.new("Cheese", "oz", 200)
+  end
 
+  it "exists" do
+    expect(@cheese).to be_a(Ingredient)
+  end
+
+  it "has a name" do
+    expect(@cheese.name).to eq("Cheese")
+  end
+
+  it "has a unit" do
+    expect(@cheese.unit).to eq("Cheese")
+  end
+
+  it "has a calorie count" do
+    expect(@cheese.calories).to eq(200)
+  end
 
 end
