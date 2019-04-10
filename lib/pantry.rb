e87c540fd1d425d434f6415dcc77a0a9b81cb98e
davisc0801 <davisc1014@gmail.com>
2019-04-10 11:17:24 -0600

    Add pantry functionality

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,23 @@
 class Pantry
 
+  attr_reader :stock
+
+  def initialize
+    @stock = Hash.new(0)
+  end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
+  end
+
+  def restock(ingredient, amount)
+    @stock[ingredient] += amount
+  end
+
+  def enough_ingredients_for?(recipe)
+    recipe.ingredients.all? do |key, value|
+      @stock[key] >= value
+    end
+  end
+
 end
